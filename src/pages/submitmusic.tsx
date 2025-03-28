e9329812be1b993de704dfc152a2141b29423b64
EminentOne1 <wongani.nyirenda@yahoo.com>
2025-03-28 10:48:19 +0200

    .

---
+++ src/pages/submitmusic.tsx
@@ -0,0 +1,19 @@
+
+
+const Submitmusic: React.FC = () => {
+
+    return (
+
+        <div className="submit-music">
+            <div className="single">
+                <span>Single</span>
+            </div>
+            <div className="album">
+                <span>Album</span>
+            </div>
+        </div>
+    )
+
+}
+
+export default Submitmusic;
--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,16 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import Root from "./root";
 import Home from "./pages/home";
 import SongPreview from "./components/SongPreview";
+import Charts from "./pages/charts";
+import Submitmusic from "./pages/submitmusic";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
       <Route path="/songPreview/:id" element={<SongPreview />} />
+      <Route path="charts" element={<Charts/>} />
+      <Route path="submit-music" element={<Submitmusic/>} />
     </Route>
   )
 );
