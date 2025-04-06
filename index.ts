6cb7f6e76d913159cc809504e5aac2d932b1f7a6
EminentOne1 <wongani.nyirenda@yahoo.com>
2025-04-06 09:09:54 +0200

    .

--- index.ts
+++ index.ts
@@ -1,14 +1,11 @@
-// server/index.ts
 import express from "express";
 import { renderPage } from "vite-plugin-ssr/server";
 
 const app = express();
 
-// ✅ Add middlewares
-app.use(express.json()); // parse JSON requests
+app.use(express.json());
 app.use("/api/custom", (req: any, res: any) => res.json({ hello: "world" })); // simple route
 
-// ✅ SSR handler
 app.get("*", async (req, res) => {
   const pageContext = await renderPage({ urlOriginal: req.originalUrl });
   const { httpResponse } = pageContext;
