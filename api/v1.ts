e2209318f8bf44c5bdbd872471f5670f95248372
EminentOne1 <wongani.nyirenda@yahoo.com>
2025-04-06 14:21:54 +0200

    .

--- api/v1.ts
+++ api/v1.ts
@@ -3,5 +3,5 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 export default function handler(req: VercelRequest, res: VercelResponse) {
   // Check if the request method is GET
 
-  res.status(405).json({ message: "this is a test api" });
+  res.status(405).json({ message: "this is a current test api" });
 }
