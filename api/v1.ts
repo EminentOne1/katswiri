ff436162ba824376b0854e0aa325b70efe5491e9
EminentOne1 <wongani.nyirenda@yahoo.com>
2025-04-06 13:25:26 +0200

    .

--- api/v1.ts
+++ api/v1.ts
@@ -1,38 +1,7 @@
-// api/v1.ts
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import express from "express";
-import { createServer } from "http";
-import { parse } from "url";
-import { IncomingMessage, ServerResponse, IncomingHttpHeaders } from "http";
 
-import songsRoutes from "../routes/songs";
-import authRoutes from "../routes/authRoutes";
-import adminRoutes from "../routes/adminRoutes";
-import sessionManager from "../middleware/sessionManager";
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // Check if the request method is GET
 
-const app = express();
-app.use(express.json());
-
-app.get("/test", (_req, res) => {
-  res.json({ message: "✅ Test route is working!" });
-});
-
-app.use("/api/v1/songs", songsRoutes);
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/adminroutes", sessionManager, adminRoutes);
-
-const server = createServer(app);
-
-export default async function handler(
-  req: VercelRequest | IncomingMessage,
-  res: VercelResponse | ServerResponse
-) {
-  if (req instanceof IncomingMessage) {
-    const parsedUrl = parse(req.url!, true);
-    (req as any).query = parsedUrl.query;
-    (req as any).headers = req.headers;
-    return app(req as any, res as any);
-  }
-
-  return app(req, res);
+  res.status(405).json({ message: "this is a test api" });
 }
